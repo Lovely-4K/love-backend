7d93ba3c23a632e05fcb5043e5d3f9b26a7f4df2
BeomSeogKim <kbs4520@daum.net>
2023-10-23 16:01:53 +0900

    feat : 테스트 Support Class 작성

---
+++ api/src/test/java/com/lovely4k/backend/ControllerTestSupport.java
@@ -0,0 +1,24 @@
+package com.lovely4k.backend;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lovely4k.backend.diary.controller.DiaryController;
+import com.lovely4k.backend.diary.service.DiaryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = {
+        DiaryController.class,
+})
+public abstract class ControllerTestSupport {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @MockBean
+    protected DiaryService diaryService;
+}
