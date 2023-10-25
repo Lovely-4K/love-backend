8a627f9f113adf74c1388af6051327be43a935b2
Jaeung Kim <kju9730@gmail.com>
2023-10-25 17:49:49 +0900

    feat : 회원 프로필 수정 Service 구현 [#35]

--- api/src/test/java/com/lovely4k/backend/member/service/MemberServiceTest.java
+++ api/src/test/java/com/lovely4k/backend/member/service/MemberServiceTest.java
@@ -2,7 +2,9 @@ package com.lovely4k.backend.member.service;
 
 import com.lovely4k.backend.member.Member;
 import com.lovely4k.backend.member.repository.MemberRepository;
+import com.lovely4k.backend.member.service.request.MemberProfileEditServiceRequest;
 import com.lovely4k.backend.member.service.response.MemberProfileGetResponse;
+import jakarta.persistence.EntityNotFoundException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -34,9 +38,9 @@ class MemberServiceTest {
         MemberProfileGetResponse memberProfileGetResponse = memberService.getMemberProfile(savedMember.getId());
 
         //then
-        Assertions.assertThat(memberProfileGetResponse)
+        assertThat(memberProfileGetResponse)
             .extracting("sex", "name")
-            .contains("boy", "김철수");
+            .contains("MALE", "김철수");
     }
 
     @Test
@@ -48,14 +52,42 @@ class MemberServiceTest {
         memberRepository.save(member);
 
         //when && then
-        Assertions.assertThatThrownBy(() -> memberService.getMemberProfile(100L))
-            .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> memberService.getMemberProfile(100L))
+            .isInstanceOf(EntityNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("회원의 프로필 정보를 수정할 수 있다.")
+    void updateMemberProfile() throws Exception {
+        //given
+        Member member = createMember();
+        Member savedMember = memberRepository.save(member);
+
+        MemberProfileEditServiceRequest serviceRequest = new MemberProfileEditServiceRequest(
+            "MALE",
+            "http://www.imageUrlSample.com",
+            "김동수",
+            "길쭉이",
+            LocalDate.of(1996, 7, 31),
+            "ENFP",
+            "blue"
+        );
+
+        //when
+        memberService.updateMemberProfile(serviceRequest, savedMember.getId());
+
+        //then
+        Member updatedMember = memberRepository.findById(savedMember.getId())
+            .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 id 입니다."));
+
+        assertThat(updatedMember).extracting("name", "nickname", "birthday")
+            .contains("김동수", "길쭉이", LocalDate.of(1996, 7, 31));
     }
 
     private Member createMember() {
         return Member.builder()
             .coupleId(1L)
-            .sex("boy")
+            .sex("MALE")
             .name("김철수")
             .nickname("듬직이")
             .birthday(LocalDate.of(1996, 7, 30))
@@ -64,6 +96,4 @@ class MemberServiceTest {
             .imageUrl("http://www.imageUrlSample.com")
             .build();
     }
-
-
 }
