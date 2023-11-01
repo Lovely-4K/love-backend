f61eeb753d52a1a480edf1fc58c53b81a6c326bb
BeomSeogKim <kbs4520@daum.net>
2023-11-01 15:01:46 +0900

    chore : yaml, docs 설정 변경

--- api/src/test/java/com/lovely4k/backend/ControllerTestSupport.java
+++ api/src/test/java/com/lovely4k/backend/ControllerTestSupport.java
@@ -12,8 +12,10 @@ import com.lovely4k.backend.question.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+@ActiveProfiles("test")
 @WebMvcTest(controllers = {
     DiaryController.class,
     MemberController.class,
