bfb1e3af249516f38c768fd21c1bdfce82bf0a07
BeomSeogKim <kbs4520@daum.net>
2023-11-03 19:15:00 +0900

    feat : Controller Logging AOP 작업

---
+++ api/src/main/java/com/lovely4k/backend/common/log/AspectControllerAdvice.java
@@ -0,0 +1,74 @@
+package com.lovely4k.backend.common.log;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Objects;
+
+@RequiredArgsConstructor
+@Aspect
+@Component
+@Slf4j
+public class AspectControllerAdvice {
+
+    private final ObjectMapper objectMapper;
+
+    private static final double MILLI_SECOND_TO_SECOND_UNIT = 0.001;
+    private static final double MAX_AFFORDABLE_TIME = 3;
+
+    /*
+     * Controller Log
+     */
+    @SneakyThrows
+    @Around("com.lovely4k.backend.common.log.Pointcuts.allController()")
+    public Object printLog(ProceedingJoinPoint joinPoint) {
+
+        ServletRequestAttributes sra = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        HttpServletRequest request = sra.getRequest();
+
+        long startTime = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long endTime = System.currentTimeMillis();
+        double elapsedTime = (endTime - startTime) * MILLI_SECOND_TO_SECOND_UNIT;
+
+        HttpServletResponse response = sra.getResponse();
+        HttpLog httpLog = HttpLog.of(request, Objects.requireNonNull(response), objectMapper, proceed);
+
+        if (elapsedTime > MAX_AFFORDABLE_TIME) {
+            log.warn(objectMapper.writeValueAsString(httpLog));
+            return proceed;
+        }
+
+        log.info(objectMapper.writeValueAsString(httpLog.removeBody()));
+
+        return proceed;
+    }
+
+    /*
+     * RestControllerAdvice Log
+     */
+    @SneakyThrows
+    @Around("com.lovely4k.backend.common.log.Pointcuts.globalExceptionHandler()")
+    public Object printErrorLog(ProceedingJoinPoint joinPoint) {
+
+        ServletRequestAttributes sra = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+
+        HttpServletRequest request = sra.getRequest();
+        Object proceed = joinPoint.proceed();
+        HttpServletResponse response = sra.getResponse();
+
+        log.warn(objectMapper.writeValueAsString(HttpLog.of(request, Objects.requireNonNull(response), objectMapper, proceed)));
+
+        return proceed;
+    }
+}
