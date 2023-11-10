c574292bd02e974a7960e949e849d6e85fefe45d
BeomSeogKim <kbs4520@daum.net>
2023-11-10 14:31:03 +0900

    fix : 테스트 코드 일부 변경

--- model/src/test/java/com/lovely4k/backend/couple/repository/RecoveryRepositoryTest.java
+++ model/src/test/java/com/lovely4k/backend/couple/repository/RecoveryRepositoryTest.java
@@ -1,7 +1,9 @@
 package com.lovely4k.backend.couple.repository;
 
 import com.lovely4k.backend.IntegrationTestSupport;
+import com.lovely4k.backend.couple.Couple;
 import com.lovely4k.backend.couple.Recovery;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,23 +19,44 @@ class RecoveryRepositoryTest extends IntegrationTestSupport {
     @Autowired
     RecoveryRepository recoveryRepository;
 
+    @Autowired
+    CoupleRepository coupleRepository;
+
+    @AfterEach
+    void tearDown() {
+        recoveryRepository.deleteAllInBatch();
+    }
+
     @DisplayName("findByCoupleId을 통해 Recovery 엔티티를 조회할 수 있다. ")
     @Test
     void findByCoupleId() {
         // given
+        Couple couple = buildCouple();
+        Couple savedCouple = coupleRepository.save(couple);
+
         LocalDate requestDate = LocalDate.of(2020, 10, 20);
-        Recovery recovery = Recovery.of(1L, requestDate);
+        Recovery recovery = Recovery.of(savedCouple.getId(), requestDate);
         recoveryRepository.save(recovery);
 
         // when
-        Optional<Recovery> optionalRecovery = recoveryRepository.findByCoupleId(1L);
+        Optional<Recovery> optionalRecovery = recoveryRepository.findByCoupleId(savedCouple.getId());
 
         // then
         assertAll(
-                () -> assertThat(optionalRecovery).isPresent(),
-                () -> assertThat(optionalRecovery.get().getCoupleId()).isEqualTo(1L),
-                () -> assertThat(optionalRecovery.get().getRequestedDate()).isEqualTo(requestDate)
+            () -> assertThat(optionalRecovery).isPresent(),
+            () -> assertThat(optionalRecovery.get().getCoupleId()).isEqualTo(savedCouple.getId())
         );
+    }
+
+    private Couple buildCouple() {
+        return Couple.builder()
+            .boyId(1L)
+            .girlId(2L)
+            .meetDay(LocalDate.of(2020, 10, 20))
+            .invitationCode("test-code")
+            .deleted(true)
+            .deletedDate(LocalDate.of(2022, 10, 20))
+            .build();
     }
 
 }
