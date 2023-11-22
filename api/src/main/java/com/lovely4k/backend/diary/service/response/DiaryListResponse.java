31f70f08d477809fa95890ee4add47a008f0aaa8
BeomSeogKim <kbs4520@daum.net>
2023-11-22 09:40:59 +0900

    refactor : diary 목록 조회시 응답 목록 변경

--- api/src/main/java/com/lovely4k/backend/diary/service/response/DiaryListResponse.java
+++ api/src/main/java/com/lovely4k/backend/diary/service/response/DiaryListResponse.java
@@ -3,21 +3,24 @@ package com.lovely4k.backend.diary.service.response;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.lovely4k.backend.diary.Diary;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record DiaryListResponse(
-        Long diaryId,
-        Long kakaoMapId,
-        String imageUrl,
-        @JsonFormat(pattern = "yyyy-MM-dd")
-        LocalDate datingDay,
-        String placeName
+    Long diaryId,
+    Long kakaoMapId,
+    String imageUrl,
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    LocalDate datingDay,
+    String placeName,
+    BigDecimal latitude,
+    BigDecimal longitude
 ) {
 
     public static DiaryListResponse from(Diary diary) {
         if (diary.getPhotos() == null) {
-            return new DiaryListResponse(diary.getId(), diary.getLocation().getKakaoMapId(), null, diary.getDatingDay(), diary.getLocation().getPlaceName());
+            return new DiaryListResponse(diary.getId(), diary.getLocation().getKakaoMapId(), null, diary.getDatingDay(), diary.getLocation().getPlaceName(), diary.getLocation().getLatitude(), diary.getLocation().getLongitude());
         }
-        return new DiaryListResponse(diary.getId(), diary.getLocation().getKakaoMapId(), diary.getPhotos().getFirstImage(), diary.getDatingDay(), diary.getLocation().getPlaceName());
+        return new DiaryListResponse(diary.getId(), diary.getLocation().getKakaoMapId(), diary.getPhotos().getFirstImage(), diary.getDatingDay(), diary.getLocation().getPlaceName(), diary.getLocation().getLatitude(), diary.getLocation().getLongitude());
     }
 }
