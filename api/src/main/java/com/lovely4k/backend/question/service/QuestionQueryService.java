25d8ad59f4fc987b4c979368f143d8333483b40c
joyfulviper <kimsk3113@naver.com>
2023-11-28 14:07:40 +0900

    feat : question, calendar 캐시 추가

--- api/src/main/java/com/lovely4k/backend/question/service/QuestionQueryService.java
+++ api/src/main/java/com/lovely4k/backend/question/service/QuestionQueryService.java
@@ -7,6 +7,7 @@ import com.lovely4k.backend.question.repository.response.QuestionDetailsResponse
 import com.lovely4k.backend.question.repository.response.QuestionGameResponse;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,14 +18,17 @@ public class QuestionQueryService {
 
     private final QuestionQueryRepository questionQueryRepository;
 
+    @Cacheable(value = "questionDetails", key = "#questionId")
     public QuestionDetailsResponse findQuestionDetails(Long questionId, Long memberId, String picture) {
         return questionQueryRepository.findQuestionDetails(questionId, memberId, picture);
     }
 
+    @Cacheable(value = "answeredQuestions", key = "#id")
     public AnsweredQuestionResponse findAllAnsweredQuestionByCoupleId(Long id, Long coupleId, Integer limit) {
         return questionQueryRepository.findAnsweredQuestions(id, coupleId, limit);
     }
 
+    @Cacheable(value = "dailyQuestions", key = "#coupleId")
     public DailyQuestionResponse findDailyQuestion(Long coupleId) {
         return questionQueryRepository.findDailyQuestion(coupleId);
     }
